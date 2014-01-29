c627b104b1be2683f35653766e061dee9b01a2a2
Ricardo Liang <ricardoliang@gmail.com>
2014-01-29 11:26:41 -0200

    Mais legível.

--- script.py
+++ script.py
@@ -1,6 +1,16 @@
-if attributes['movx'] == 0:
-    attributes['movx'] = 1
-elif attributes['posx'] >= 10:
-    attributes['movx'] = -1
-elif attributes['posx'] <= -10:
-    attributes['movx'] = 1
+posx = attributes['posx']
+posy = attributes['posy']
+movx = attributes['movx']
+movy = attributes['movy']
+
+def move(x=movx, y=movy):
+    attributes['movx'] = x
+    attributes['movy'] = y
+
+
+if movx == 0 and movy == 0:
+    move(1, 1)
+elif posx <= -10 and posy <= -10:
+    move(1, 1)
+elif posx >= 10 and posy >= 10:
+    move(-1, -1)
