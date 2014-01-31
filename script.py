708e83437baf9dc9496b01833dd876811ea8887e
Ricardo Liang <ricardoliang@gmail.com>
2014-01-31 17:33:09 -0200

    Modificações no script.

--- script.py
+++ script.py
@@ -1,15 +1,18 @@
 import random
 
-if attributes['posx'] <= -10:
+attributes['lookx'] = random.randrange(-1, 2)
+attributes['looky'] = random.randrange(-1, 2)
+
+if attributes['posx'] <= -20:
     attributes['movx'] = 1
-elif attributes['posx'] >= 10:
+elif attributes['posx'] >= 20:
     attributes['movx'] = -1
 else:
     attributes['movx'] = random.randrange(-1, 2)
 
-if attributes['posy'] <= -10:
+if attributes['posy'] <= -20:
     attributes['movy'] = 1
-elif attributes['posy'] >= 10:
+elif attributes['posy'] >= 20:
     attributes['movy'] = -1
 else:
     attributes['movy'] = random.randrange(-1, 2)
